d0d260ebd5b00c651ae20cb26a3a095b4876af29
stefanleithenmayr <s.leithenmayr@gmx.at>
2019-02-12 17:31:47 +0100

    Add files via upload

---
+++ RWProblem/src/rwproblem/FXMLDocumentController.java
@@ -0,0 +1,18 @@
+
+package rwproblem;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ *
+ * @author sleit
+ */
+public class FXMLDocumentController implements Initializable {
+        
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }    
+}
---
+++ RWProblem/src/rwproblem/RWProblem.java
@@ -0,0 +1,34 @@
+
+package rwproblem;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author sleit
+ */
+public class RWProblem extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        stage.setTitle("RW - Problem");
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
